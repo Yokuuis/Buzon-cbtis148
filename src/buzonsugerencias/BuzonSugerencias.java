26bc9611c1579240b1ad69dd6c360cda05af109c
Kipaert <kipaerto@gmail.com>
2025-11-23 11:37:53 -0600

    repositorio para lizz 300 pesitos para la comida de la semana :vvvv

--- src/buzonsugerencias/BuzonSugerencias.java
+++ src/buzonsugerencias/BuzonSugerencias.java
@@ -1,8 +1,30 @@
 package buzonsugerencias;
 
+import buzonsugerencias.view.FrmMenuPrincipal;
+
 public class BuzonSugerencias {
+
     public static void main(String[] args) {
 
+        // (Opcional) Dejar Nimbus si existe
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info :
+                    javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // si falla, se queda el look and feel por defecto
+        }
+
+        // Lanzar la ventana principal
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new FrmMenuPrincipal().setVisible(true);
+            }
+        });
     }
-    
 }
